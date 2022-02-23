223b4d3ce85a5e3bf8185d1119bb3d5ec58d434e
takker99 <37929109+takker99@users.noreply.github.com>
2022-02-23 18:05:06 +0900

    :bug: wrong path of deno.json

--- api/convert.ts
+++ api/convert.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env deno run --config ../deno.json
+
 import { ServerRequest } from "https://deno.land/std@0.105.0/http/server.ts";
 import { fetchText } from "../src/fetch.ts";
 import { makeSVG } from "../src/makeSVG.tsx";
